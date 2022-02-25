fc4b562c3ea47dc7d8d00910d221d290112458ae
Antoni Minkiewicz <antoni@antoni-air.local>
2022-02-25 19:39:10 +0000

    initial commit

---
+++ field/LocationSensor.java
@@ -0,0 +1,27 @@
+package field;
+/*
+ * Created on Feb 2022
+ */
+import common.Location;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class LocationSensor /* TODO extend appropriate classes and implement the appropriate interfaces */ {
+    private Location location;
+
+    public LocationSensor () throws RemoteException {
+        super();
+        location = new Location(40.831436, 14.242832);
+    }
+
+    public LocationSensor(Location location) throws RemoteException {
+        super();
+        this.location = location;
+    }
+
+    @Override
+    public Location getCurrentLocation () throws RemoteException {
+        return location;
+    }
+}
---
+++ src/test/java/CentralServerTest.java
@@ -0,0 +1,9 @@
+package src.test.java;
+
+import org.junit.jupiter.api.Test;
+
+public class CentralServerTest {
+
+  @Test
+  def centralServer
+}
